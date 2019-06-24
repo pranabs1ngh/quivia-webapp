9345a5f0a39837db051e830a25228e2770cb6922
Pranab Singh <iampranabb@gmail.com>
2019-06-25 00:03:56 +0530

    fixed bugs

--- server/config/socket.js
+++ server/config/socket.js
@@ -1,15 +1,24 @@
+const axios = require('axios');
+
 const rooms = [];
 const socketRoomMap = [];
 
 const findRoom = term => rooms.findIndex(room => room.id.startsWith(term));
+const searchForRoom = term => {
+  for (let i = 0; i < rooms.length; i++) {
+    if (rooms[i].id.startsWith(term) && rooms[i].length < 2)
+      return i
+  }
+  return -1;
+};
 
 const socket = io => {
   io.on('connection', socket => {
     console.log('New User Connected...');
 
     socket.on('search_room', topic => {
-      const index = findRoom(topic);
-      if (index + 1 && rooms[index].length < 2)
+      const index = searchForRoom(topic);
+      if (index + 1)
         socket.emit('room_found', rooms[index].id);
       else socket.emit('room_not_found', null);
     })
@@ -29,6 +38,13 @@ const socket = io => {
 
       socketRoomMap.push({ socketID: socket.id, roomID: roomID });
       io.to(roomID).emit('opponent_found', rooms[index]);
+    })
+
+    socket.on('send_questions', async roomID => {
+      const index = findRoom(roomID);
+
+      const questions = await axios.get(`https://opentdb.com/api.php?amount=7&category=${rooms[index].key}&type=multiple`);
+      io.to(roomID).emit('receive_questions', questions);
     })
 
     socket.on('disconnect', () => {
