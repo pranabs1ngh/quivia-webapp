0c6dbf2ac509eb6ace846f7c8ff1685ae931fc53
Pranab Singh <iampranabb@gmail.com>
2019-07-09 15:34:22 +0530

    embedded audio

---
+++ server/config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  cookieSecret: process.env.COOKIE_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  gooogleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookClientID: process.env.FACEBOOK_APP_ID,
+  facebookClientSecret: process.env.FACEBOOK_APP_SECRET,
+};
