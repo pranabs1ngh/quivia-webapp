a88f922da703bf9600095e72f7fa49c236d0c933
Pranab Singh <iampranabb@gmail.com>
2025-07-25 03:42:43 +0530

    minor fixes

--- server/index.js
+++ server/index.js
@@ -72,8 +72,8 @@ require('./services/socket')(socket(server, {
   origins: allowedOrigins,
   handlePreflightRequest: (req, res) => {
     const headers = {
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        "Access-Control-Allow-Origin": req.headers.origin,
+        "Access-Control-Allow-Methods": "GET,POST",
+        "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Credentials": true
     };
     res.writeHead(200, headers);
