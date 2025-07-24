2287e5fde3a5270ac27672a7dee765adfe59e87d
Pranab Singh <iampranabb@gmail.com>
2025-07-24 23:02:52 +0530

    minor changes

--- server/config/keys.js
+++ server/config/keys.js
@@ -2,7 +2,7 @@ module.exports = {
   appBackendURL: process.env.APP_BACKEND_URL,
   appURL: process.env.APP_URL,
   cookieSecret: process.env.COOKIE_SECRET,
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: process.env.MONGODB_URI,
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   gooogleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 };
